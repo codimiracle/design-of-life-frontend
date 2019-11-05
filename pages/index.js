fdff71b897dee47b3c1f57c413adae1bc384716e
codimiracle <codimiracle@outlook.com>
2019-11-05 19:32:05 +0800

    applied default layout to root page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,47 @@
-function Home() {
-    return (<div>Design your life and make it powerful!</div>);
-}
+import AwardPanel from '../components/award-panel';
+import RecentPanel from '../components/recent-panel';
+import LifeDesignPanel from '../components/life-design-panel';
+import DefaultLayout from '../layout/default-layout';
+
+const awards = [];
+const recents = [];
+
+const Home = () => (
+    <DefaultLayout>
+        <aside className="aside-bar">
+            <section className="award-area">
+                <AwardPanel awards={awards} />
+            </section>
+            <section className="recent-area">
+                <RecentPanel recents={recents} />
+            </section>
+        </aside>
+        <article className="main">
+            <LifeDesignPanel />
+        </article>
+        <style jsx>{`
+            .aside-bar {
+                width: 348px;
+            }
+            section + section {
+                margin-top: 32px;
+            }
+            .main {
+                margin-left: 32px;
+                flex: 1;
+            }
+        `}</style>
+        <style jsx global>{`
+            html, body {
+                margin: 0;
+            }
+            .panel {
+                padding: 12px;
+                border-radius: 12px;
+                box-shadow: 0 0 20px 2px lightgrey;
+            }
+        `}</style>
+    </DefaultLayout>
+);
 
 export default Home;
