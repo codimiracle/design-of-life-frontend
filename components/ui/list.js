6af47a76358e78cb5fd909e57fd1f83a14e59802
codimiracle <codimiracle@outlook.com>
2019-11-12 23:09:17 +0800

    add list component

--- components/ui/list.js
+++ components/ui/list.js
@@ -1,21 +1,119 @@
+import OptionBar from "./option-bar";
+import Checkbox from "./checkbox";
+import Button from "./button";
+function getDescriptor(items, settings) {
+    const metadata = (settings || {}).metadata || {};
+    const descriptor = {
+        items: items,
+        metadata: metadata,
+        keys: Object.keys(metadata),
+        describes: []
+    };
+    items.forEach(item => {
+        let describe = {
+            item: item,
+            mapping: {}
+        }
+        descriptor.keys.forEach(key => {
+            let types = (metadata[key] || {}).type || [];
+            types.forEach(type => {
+                describe.mapping[type] = item[key];
+            });
+        });
+        descriptor.describes.push(describe);
+    });
+    return descriptor;
+}
+
 class List extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            selectedKey: null
+        };
+        this.descriptor = getDescriptor(this.props.items, this.props.settings);
     }
-    renderItem(item) {
-        return <li>{this.props.itemRender(item)}</li>
+    renderField(item, field, fieldmeta) {
+        return (fieldmeta.render || function () { return '' })(item);
+    }
+    renderItem(describe) {
+        const descriptor = this.descriptor;
+        const selectedKey = this.state.selectedKey;
+        const { item, mapping } = describe;
+        return (
+            <li key={mapping['item.key']} className={selectedKey == mapping['item.key'] ? '--active' : ''}>
+                <div className="list-item">
+                    <Checkbox />
+                    <div className="list-item-renderred">
+                        {
+                            descriptor.keys.map((key) => this.renderField(item, key, descriptor.metadata[key]))
+                        }
+                    </div>
+                </div>
+                <style jsx>{`
+                    li {
+                        padding: 6px 0;
+                        transition: all 200ms linear;
+                    }
+                    .list-item {
+                        display: flex;
+                    }
+                    .list-item-renderred {
+                        flex: 1;
+                    }
+                    li:hover, li.--active {
+                        background-color: lightgrey;
+                    }
+                    li + li {
+                        margin-top: 3px;
+                    }
+                    li + li::before {
+                        content: '';
+                        display: block;
+                        border-top: 1px solid lightgrey;
+                        position: relative;
+                        top: -8px;
+                    }
+                `}</style>
+            </li>
+        );
+    }
+    renderOption(describe) {
+        const mapping = describe.mapping;
+        return (<option key={mapping['item.key']} value={mapping['item.value']}>{mapping['item.name']}</option>);
     }
     render() {
-        const items = this.props.items;
+        const descriptor = this.descriptor;
+        const selectAll = <Checkbox />;
+        const menu = <Button icon="bars" />
         return (
-            <>
+            <div className="list-component">
+                <select multiple onChange={(e) => { this.setState({ selectedKey: e.target.value }) }}>
+                    {descriptor.describes.map(describe => this.renderOption(describe))}
+                </select>
+                <OptionBar title={selectAll} operator={menu} />
                 <ul>
-                    {items.map(item => this.renderItem(item))}
+                    {descriptor.describes.map(describe => this.renderItem(describe))}
                 </ul>
-                <style jsx>
-
-                </style>
-            </>
+                <style jsx>{`
+                    .list-component {
+                        position: relative;
+                    }
+                    .list-component:focus-within {
+                        outline: 1px solid deepskyblue;
+                    }
+                    ul {
+                        list-style-type: none;
+                        margin: 0;
+                        padding: 0;
+                    }
+                    select {
+                        opacity: 0;
+                        position: absolute;
+                        z-index: -1;
+                    }
+                `}</style>
+            </div>
         );
     }
 }
