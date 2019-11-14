fb396f794db619972b4b0898ec2e07dcc05550d9
codimiracle <codimiracle@outlook.com>
2019-11-14 15:02:33 +0800

    polish option bar component

--- components/ui/option-bar.js
+++ components/ui/option-bar.js
@@ -9,6 +9,9 @@ const OptionBar = (props) => (
         <style jsx>{`
             .option-bar {
                 display: flex;
+                padding: 4px 0;
+                border-bottom: 1px solid lightgrey;
+                margin: 1px;
                 justify-content: space-between;
             }
         `}</style>
