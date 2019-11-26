341dd9f301dd060b01eb689382c6fac66252057c
codimiracle <codimiracle@outlook.com>
2019-11-26 13:07:07 +0800

    merge into _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import '../config/icon-config';
 import App from 'next/app';
 
 class DolApp extends App {
+    componentDidMount() {
+        document.addEventListener('selectstart', function (e) { e.preventDefault(); });
+    }
     render() {
         const { Component, pageProps } = this.props;
         return (
